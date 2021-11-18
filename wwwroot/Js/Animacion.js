4d7c2bbd6174024636a44784a5e8a45b4ce8f7d5
ChellBurgos <93162745+ChellBurgos@users.noreply.github.com>
2021-11-17 23:36:13 -0600

    nav

---
+++ wwwroot/Js/Animacion.js
@@ -0,0 +1,26 @@
+﻿let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e) => {
+        let arrowParent = e.target.parentElement.parentElement;
+        arrowParent.classList.toggle("showMenu");
+    });
+}
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+});
+
+let list = document.querySelectorAll('.list');
+for (let i = 0; i < list.length; i++) {
+    list[i].onclick = function () {
+        let j = 0;
+        while (j < list.length) {
+            list[j++].className = 'list';
+        }
+        list[i].className = 'list active';
+    }
+}
+
